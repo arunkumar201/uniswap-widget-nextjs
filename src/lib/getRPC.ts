dca8dcd0dfd1b2f60f23693415ffdf6a2de9245c
Arun Kumar <arunprajapati9825@gmail.com>
2024-05-30 07:28:23 +0530

    updated rpcs

--- src/lib/getRPC.ts
+++ src/lib/getRPC.ts
@@ -1,9 +1,6 @@
 const RPC_LIST_ETH = [
 	"https://eth.llamarpc.com",
-	"https://ethereumnodes.com/",
 	"https://eth.public-rpc.com",
-	"https://api.zan.top/node/v1/eth/mainnet/public",
-	"https://eth-pokt.nodies.app",
 	"https://rpc.builder0x69.io",
 	"https://eth-mainnet.g.alchemy.com/v2/QWnDJnnZFRJSZABxedDHL1l1x2Y3c1aR",
 ];
